f076961715e968e4b59f20b955d7cd4b98e5c31d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-02 13:44:06 -0800

    add User

--- src/com/caresoft/clinicapp/User.java
+++ src/com/caresoft/clinicapp/User.java
@@ -2,8 +2,29 @@ package com.caresoft.clinicapp;
 
 public class User {
 
-	public User() {
-		// TODO Auto-generated constructor stub
+	protected Integer id;
+	protected int pin;
+	
+	//	//// CONSTRUCTOR ////////////////////////////////////////////
+	
+	public User(Integer id) {
+		this.id = id;
 	}
+	
+	//	//// GET AND SET ////////////////////////////////////////////
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+		
 
 }
